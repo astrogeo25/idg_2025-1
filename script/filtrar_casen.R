e9c0b70c860193a2a0f2891d7a62bd1ce85759a4
astrogeo25 <astroza15m@gmail.com>
2025-04-20 23:40:03 -0400

    Analisis y conclusión

--- script/filtrar_casen.R
+++ script/filtrar_casen.R
@@ -1,20 +1,23 @@
-# Abrir CASEN
-# Entradas
+library(corrplot)
 ruta_rds = "data/casen_rm.rds"
 casen_rm = readRDS(ruta_rds)
 
 # Histograma de ingreso per capita original
-hist(casen_rm$ypc, xlab = "Ingreso percapita", col = "lightblue")
-boxplot(casen_rm$ypc)
+#hist(casen_rm$ypc, xlab = "Ingreso percapita", col = "lightblue")
+#boxplot(casen_rm$ytotcor)
 
 # Reducción del umbral
-umbral = quantile(casen_rm$ypc, 0.85, na.rm = TRUE)
-casen_clean = casen_rm[casen_rm$ypc <= umbral & (casen_rm$edad > 15) & (casen_rm$edad < 65), ]
-boxplot(casen_clean$ypc)
-hist(casen_clean$ypc, xlab = "Ingreso percapita", col = "green")
+umbral = quantile(casen_rm$ytotcor, 0.9, na.rm = TRUE)
+clean = casen_rm[casen_rm$ytotcor <= umbral & (casen_rm$edad < 65), ]
+#hist(clean$ypc, xlab = "Ingreso percapita", col = "green")
+#boxplot(clean$ytotcor)
 
-# Crear el modelo de regresión multiple y resumir o25
-modelo <- lm(casen_clean$ypc ~ casen_clean$edad + casen_clean$e6a , data = casen_clean)
+# Creación de data frame y matriz de correlación
+tabla <- data.frame(clean$ytotcor, clean$edad, clean$e6a, clean$p9)
+correlation_matrix <- cor(tabla, use = "complete.obs", method = "pearson")
+#Gráfica
+corrplot(correlation_matrix, method = "color", tl.cex = 0.8, number.cex = 0.7)
+
+# Crear el modelo de regresión multiple
+modelo <- lm(clean$ytotcor ~ clean$edad + clean$e6a + clean$p9, data = clean)
 summary(modelo)
-
-# Intepretar beta, signo, significancia
